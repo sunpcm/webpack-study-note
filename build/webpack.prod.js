1ea8c0266ae9928010bcb63a3649922110facae9
sunpcm <sunpcm@163.com>
2025-12-29 16:21:44 +0800

    feat: enhance Webpack production configuration with MiniCssExtractPlugin and BundleAnalyzerPlugin

--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -1,19 +1,27 @@
 const { merge }  = require('webpack-merge')
-const common = require('webpack.common')
+const common = require('./webpack.common.js')
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+
 
 module.exports = merge(common, {
 	mode: 'production',
 	devtool: 'source-map',
 	output: {
-		path: path.resolve(__dirname, '..dist''),
+		path: path.resolve(__dirname, '../dist'),
 		filename: '[name].[contenthash].js',
 		clean: true,
 	},
 	plugins: [
 		new MiniCssExtractPlugin({
 			filename: '[name].[contenthash].css'
+		}),
+		new BundleAnalyzerPlugin({
+			// analyzerMode: 'server', // 默认模式：启动一个 HTTP 服务器展示
+			analyzerMode: 'static', // 推荐模式：生成一个 HTML 文件 (不占用端口，方便查看)
+			openAnalyzer: true,     // 构建完自动打开浏览器
+			reportFilename: 'bundle-report.html', // 生成的文件名
 		}),
 	],
 	module: {
@@ -23,7 +31,7 @@ module.exports = merge(common, {
 				use: [
 					MiniCssExtractPlugin.loader,
 					'css-loader',
-					'scss-loader'
+					'sass-loader'
 				]
 			}
 		]
